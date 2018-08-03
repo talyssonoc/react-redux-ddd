33fe877ae79022388a298d8db509b1e01e159497
Talysson <talyssonoc@gmail.com>
2018-08-03 13:26:51 -0300

    Name home page accordingly

--- src/view/Router.js
+++ src/view/Router.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './home/Home';
+import HomePage from './home/HomePage';
 import Layout from './layout/Layout';
 import LoginPage from './auth/LoginPage';
 import RegisterPage from './auth/RegisterPage';
@@ -9,7 +9,7 @@ const Router = () => (
   <BrowserRouter>
     <Layout>
       <Switch>
-        <Route path='/' exact component={ Home } />
+        <Route path='/' exact component={ HomePage } />
         <Route path='/login' component={ LoginPage } />
         <Route path='/register' component={ RegisterPage } />
       </Switch>
